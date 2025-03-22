3d0de615c9d857db01b7ce2170e4ae41560ed675
Shiva Ragannagari <ragannagarishiva@gmail.com>
2025-03-22 22:01:43 +0530

    Add files via upload

---
+++ JavaScript/CalculatorApp.js
@@ -0,0 +1,9 @@
+import{add,sub,mul,div} from"./Calculator.js"
+let addres=add(10,20)
+let subres=sub(20,5)
+let mulres=mul(23,4)
+let divres=div(20,25)
+console.log("Add result: "+addres)
+console.log("Sub result: "+subres)
+console.log("Mul result: "+mulres)
+console.log("Div result: "+divres)
